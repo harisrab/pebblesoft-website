636b96eb53df576f746c7e7decc3deb0cd10f967
Haris Rashid <62747193+harisrab@users.noreply.github.com>
2022-12-27 10:08:21 +0000

    Added Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
 import "../styles/globals.css";
+import Script from "next/script";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-50L29BF4PE"
+      ></Script>
+      <Script
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+            `,
+        }}
+        strategy="afterInteractive"
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
